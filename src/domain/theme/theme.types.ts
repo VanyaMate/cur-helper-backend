f037b76695e044760fbc4f22c1c3078a5565036f
VanyaMate <buhu653@mail.ru>
2024-01-10 14:30:39 +0300

    update theme types

--- src/domain/theme/theme.types.ts
+++ src/domain/theme/theme.types.ts
@@ -1,16 +1,37 @@
 import { TestResultData } from '../test/test.types';
 
 
-export type Theme = {
-    id: string;
-    title: string;
-    description: string;
-    additional: string;
-    url: string[];
-}
+export type Theme =
+    {
+        id: string;
+        title: string;
+        description: string;
+        url: string[];
+    }
 
-export type ThemeWithTest =
+export type ThemeBody =
+    {
+        body: string;
+    }
+
+export type ThemeWithTestResult =
     {
         test: TestResultData;
     }
     & Theme;
+
+export type ThemeWithChildren =
+    {
+        children: Theme[];
+    }
+    & Theme;
+
+export type ThemeWithTests =
+    {
+        children: ThemeWithTestResult[];
+    }
+    & ThemeWithTestResult;
+
+export type ThemeData =
+    Theme
+    & ThemeBody;
