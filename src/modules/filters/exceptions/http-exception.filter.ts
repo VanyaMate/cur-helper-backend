118e3b7f00c2d473ed54aa339ce2b14599b51f87
VanyaMate <buhu653@mail.ru>
2024-04-25 04:20:10 +0300

    update exception filter. НУЖЕН РЕФАКТОРТИНГ

--- src/modules/filters/exceptions/http-exception.filter.ts
+++ src/modules/filters/exceptions/http-exception.filter.ts
@@ -2,7 +2,7 @@ import {
     ExceptionFilter,
     Catch,
     ArgumentsHost,
-    HttpStatus,
+    HttpStatus, HttpException,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ErrorMessageType } from '@vanyamate/cur-helper-types';
@@ -12,6 +12,35 @@ import {
 import { ErrorResponseType } from '@vanyamate/cur-helper-types/types/error';
 
 
+const isErrorResponseType = function (error: unknown): error is ErrorResponseType {
+    if (typeof error !== 'object') {
+        return false;
+    }
+
+    if (
+        !Array.isArray(error['errors']) ||
+        typeof error['code'] !== 'number'
+    ) {
+        return false;
+    }
+
+    return true;
+};
+
+const isHttpExceptionError = function (error: unknown): error is HttpException {
+    if (typeof error !== 'object') {
+        return false;
+    }
+    if (
+        typeof error['response'] !== 'string' ||
+        typeof error['status'] !== 'number'
+    ) {
+        return false;
+    }
+
+    return true;
+};
+
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
     constructor (
@@ -19,15 +48,27 @@ export class HttpExceptionFilter implements ExceptionFilter {
     ) {
     }
 
-    catch (exception: ErrorResponseType, host: ArgumentsHost) {
+    catch (exception: unknown, host: ArgumentsHost) {
         const ctx      = host.switchToHttp();
         const response = ctx.getResponse<Response>();
-        if (exception.errors && exception.code) {
+        if (isErrorResponseType(exception)) {
             const code: HttpStatus = this._errorTypeConverter.to(exception.code);
             response
                 .status(code)
                 .json({
                     errors: exception.errors,
+                });
+        } else if (isHttpExceptionError(exception)) {
+            const code: HttpStatus = this._errorTypeConverter.to(exception.getStatus());
+            response
+                .status(code)
+                .json({
+                    errors: [
+                        {
+                            target  : '',
+                            messages: exception.getResponse(),
+                        },
+                    ],
                 });
         } else {
             response
